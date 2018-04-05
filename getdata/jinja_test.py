b3020c9fc3d055d7d9123ac220126e817fb36060
Mo Zebdi <mozebdi@no-reply.users.github.com>
2018-04-05 17:25:20 -0400

    fixed detail with primary keys in equivalency tables
    also built a somewhat more robust template for model generation with multiple datasets and indicators, one indicator per table

--- getdata/jinja_test.py
+++ getdata/jinja_test.py
@@ -12,10 +12,10 @@ ind.column = 'pop_100'
 
 
 env = Environment(
-    loader=FileSystemLoader('./templated_models.py'),
+    loader=FileSystemLoader('./template_models.py.jinja'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-modelTemplate = env.get_template('templated_models.py')
+modelTemplate = env.get_template('template_models.py.jinja')
 
 print(modelTemplate.render(dataset_name='Population', indicator=ind))
