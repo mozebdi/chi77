08bb2cddcac9f24d67bca243415347aecbaed14b
Mo Zebdi <mozebdi@no-reply.users.github.com>
2018-09-09 16:01:36 -0400

    MDP-39: Added vue.js components necessary to convert app (mozebdi)

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <v-map>
+
+    </v-map>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import { InfoControl, ReferenceChart, ChoroplethLayer } from 'vue-choropleth';
+import Vue2Leaflet from 'vue2-leaflet';
+
+export default {
+  name: 'app',
+  components: {
+    'v-map': Vue2Leaflet.map,
+    'v-info-control': InfoControl,
+    'v-reference-chart': ReferenceChart,
+    'v-choropleth-layer': ChoroplethLayer,
+  },
+  mounted() {
+
+  },
+};
+</script>
+<style>
+@import '~leaflet/dist/leaflet.css';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
